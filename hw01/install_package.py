cd45dc9d03099f568d8f646cc9df3cdb7c6f1902
gusrlLee <gusrl3204@naver.com>
2023-03-18 20:15:29 +0900

    edit file

--- hw01/install_package.py
+++ hw01/install_package.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+import sklearn
 
+print(" Pandas Version : ", pd.__version__)
+print(" Numpy Version : ", np.__version__)
+print(" SciKit Learn Version : ", sklearn.__version__)
 print(" SUCCESS!! ")
-
